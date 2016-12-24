e3665807ae0298f46a1bf792798fdfc573c4cfa0
karmi214 <karmi214@gmail.com>
2016-12-24 13:44:57 +0545

    test
    test

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include("includes/htmldocu.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sajan chor</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
+    <script src="css/jquery/jquery.min.js"></script>
+  <script src="css/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+<?php
+webheaderAndsearch();
+ ?>
+
+ </body>
+</html>
